bc941979a495437eca75b2193d3795cc534a6fcc
wolf3d <oldcar@inbox.lv>
2020-07-30 10:36:38 +0300

    some test stuff added

--- src/inject.js
+++ src/inject.js
@@ -1,3 +1,10 @@
 console.log("toast toast 123");
-var matches = document.querySelectorAll('span[role="text"]:not([class])');
-console.log(matches);
+//var matches = document.querySelectorAll('span[role="text"]:not([class])');
+var matches = document.querySelectorAll('div[class="kur-room__content"]');
+var first = matches[0];
+var element = document.createElement("aside");
+element.setAttribute("class", "fact widget brief lp_fact");
+element.textContent = "some text";
+//class="fact widget brief lp_fact"
+first.appendChild(element);
+console.log(first);
